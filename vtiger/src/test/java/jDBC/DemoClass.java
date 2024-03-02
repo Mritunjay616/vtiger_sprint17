a99c684b97c11b197f92105713e7f33f9782c5a0
Mritunjay616 <mritunjaykumar616@gmail.com>
2024-03-02 19:18:18 +0530

    second time changes from TE1

--- vtiger/src/test/java/jDBC/DemoClass.java
+++ vtiger/src/test/java/jDBC/DemoClass.java
@@ -2,6 +2,6 @@ package jDBC;
 public class DemoClass {
 
 	public static void main(String[] args) {
-		System.out.println("Drmo push from new branch");
+		System.out.println("Demo push from new branch");
 	}
 }
