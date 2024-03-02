d813995c4263fa2d7f0862848f1674d77ca291db
Mritunjay616 <131840989+Mritunjay616@users.noreply.github.com>
2024-03-02 20:02:35 +0530

    Update DemoClass.java

--- vtiger/src/test/java/jDBC/DemoClass.java
+++ vtiger/src/test/java/jDBC/DemoClass.java
@@ -3,6 +3,7 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		System.out.println("Demo push from new branch");
-		System.out.println("Demo changes in global");
+		System.out.println("Demo 1st changes in global");
+		System.out.println("Demo 2nd changes in global");
 	}
 }
